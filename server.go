bc117dfb0d6c5f28165b3b3fd33a8ef128f8c4e0
Kazuki Chinone <staybesidehangon@gmail.com>
2020-10-14 14:18:08 +0900

    tweak

--- server.go
+++ server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"github.com/sunoko/youtube-manager-go/routes"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Fatal("Error loading .env")	
+	}
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 }
@@ -17,6 +22,7 @@ func main() {
 
 	// Middlewares
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
 	
 	// Routes
 	routes.Init(e)
