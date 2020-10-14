1824a8f8e1bfa43b2de3e51341a974346f58042e
Kazuki Chinone <staybesidehangon@gmail.com>
2020-10-14 13:57:17 +0900

    tweak

--- server.go
+++ server.go
@@ -3,10 +3,20 @@ package main
 import (
 	"github.com/sunoko/youtube-manager-go/routes"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 )
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
 
 func main() {
 	e := echo.New()
+
+	// Middlewares
+	e.Use(middleware.Logger())
 	
 	// Routes
 	routes.Init(e)
